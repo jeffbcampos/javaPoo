9e1f8410ed53e916ea226a09e7348f1cdc7ea514
Jeferson Campos <jefbcampos@gmail.com>
2023-05-28 16:43:34 -0300

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         cachorro1 = new Cachorro("dog", "caramelo", 25, 5.5, 5);
 
         System.out.println(cachorro1.getNome());
-        cachorro1.latir();
+        cachorro1.soar();
         System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
         System.out.println("Pega a bolinha " + cachorro1.getNome());
         System.out.println("O " + cachorro1.getNome() + " " + cachorro1.interagir("pega"));
